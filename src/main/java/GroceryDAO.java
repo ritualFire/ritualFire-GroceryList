a45ec122bd8b750b63e3b106b93ad5abb9e1d6ae
ritualFire <jpelaez456@gmail.com>
2023-03-05 19:23:38 +0000

    GroceryList

--- src/main/java/GroceryDAO.java
+++ src/main/java/GroceryDAO.java
@@ -28,7 +28,7 @@ public class GroceryDAO {
         List<String> groceries = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "Select * from Grocery";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -60,10 +60,12 @@ public class GroceryDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "Insert into Grocery(grocery_name) Values (?)";
             PreparedStatement ps = connection.prepareStatement(sql);
 
             //add code that leverages ps.setString here
+
+            ps.setString(1,groceryName);
 
             ps.executeUpdate();
         }catch(SQLException e){
